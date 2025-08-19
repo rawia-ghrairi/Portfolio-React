0d368fcc469cea4f169030b59f38ca39c7b6436f
unknown <rawiaghrairi@gmail.com>
2025-08-19 13:21:25 +0100

    Initial commit - Portfolio project with react

---
+++ src/components/Services/Services.jsx
@@ -0,0 +1,89 @@
+import React, { useContext } from "react";
+import "./Services.css";
+import Card from "../Card/Card";
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
+import { themeContext } from "../../Context";
+import { motion } from "framer-motion";
+import Resume from './resume.pdf';
+
+const Services = () => {
+  // context
+  const theme = useContext(themeContext);
+  const darkMode = theme.state.darkMode;
+
+  // transition
+  const transition = {
+    duration: 1,
+    type: "spring",
+  };
+
+  return (
+    <div className="services" id="services">
+      {/* left side */}
+      <div className="awesome">
+        {/* dark mode */}
+        <span style={{ color: darkMode ? "white" : "" }}>My Awesome</span>
+        <span>services</span>
+        <spane>
+          Web Development & DevOps services to help you build,
+          <br />
+          deploy, and scale your projects efficiently.
+        </spane>
+        <a href={Resume} download>
+          <button className="button s-button">Download CV</button>
+        </a>
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+      </div>
+      {/* right */}
+      <div className="cards">
+        {/* first card */}
+        <motion.div
+          initial={{ left: "25rem" }}
+          whileInView={{ left: "14rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={HeartEmoji}
+            heading={"Mobile Developer"}
+            detail={"React Native, Ionic"}
+          />
+        </motion.div>
+        {/* second card */}
+        <motion.div
+          initial={{ left: "-11rem", top: "12rem" }}
+          whileInView={{ left: "-4rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Glasses}
+            heading={"Web Developer"}
+            detail={"Html, Css, JavaScript, React, Nodejs, Angular,Next js,Tailwind CSS, Bootstrap"}
+          />
+        </motion.div>
+        {/* 3rd */}
+        <motion.div
+          initial={{ top: "19rem", left: "25rem" }}
+          whileInView={{ left: "12rem" }}
+          transition={transition}
+        >
+          <Card
+            emoji={Humble}
+            heading={"DevOps"}
+            detail={
+              "Docker, Kubernetes, CI/CD, GitLab, AWS, Monitoring (Grafana, Kibana)"
+            }
+            color="rgba(252, 166, 31, 0.45)"
+          />
+        </motion.div>
+        <div
+          className="blur s-blur2"
+          style={{ background: "var(--purple)" }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Services;
